bb03393d84ba5d40a612253b7284115b03baaf0d
Liaess <yann.melo@gmail.com>
2022-01-13 15:18:10 -0300

    Preparando revisão de código

--- src/components/Logos/MiniLogo/index.js
+++
@@ -1,5 +0,0 @@
-import logo from "../../../assets/images/MiniLogo.png"
-
-export default function MiniLogo(){
-    return <img src={logo} alt="mini logo"></img>
-}
--- src/components/UserImage/index.js
+++
@@ -1,5 +0,0 @@
-import { Img } from "./styles"
-
-export default function UserImage({img}){
-    return <Img src={img} alt="user image"/>
-}
